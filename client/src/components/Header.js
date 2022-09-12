3d2404bcdcc19a86741e74582ee2dba08309fee4
Ethan Hasbrouck <ethanhasbrouck02@gmail.com>
2022-09-11 20:28:33 -0400

    Style update

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,18 @@
 import React from 'react';
+
+import 'material-icons/iconfont/material-icons.css';
 const Header = (props) => {
 
     return (
         <div className='header'>
 
-                <span className="material-symbols-rounded settings">
+                <span className="material-icons settings">
                 settings
                 </span>
-                            <a href="/"className='title'>
-                <h1 className={props.darkMode}>zerl<span className="material-symbols-outlined">public</span>g</h1>
-            </a>
-                <span className="material-symbols-rounded settings">
+                <a href="/">
+                <h1 className={props.darkMode}>zerl<span className="material-icons-outlined globe">public</span>g</h1>
+                </a>
+                <span className="material-icons settings">
                 person
                 </span>
         </div>
--- client/src/components/Sector.js
+++ client/src/components/Sector.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import SectorItem from "./SectorItem";
 import Modal from './Modal';
-import "./styles/Card.css"
+import "./styles/Card.css";
+import 'material-icons/iconfont/material-icons.css';
 
 const Sector = (props) => {
     const [isOpen, setIsOpen] = useState(false);
     return (
         <div className="content-block" onMouseEnter={() => setIsOpen(true)} onMouseLeave={() => setIsOpen(false)}>
-            <span className="material-symbols-outlined icon">
+            <span className="material-icons-outlined icon">
                 {props.icon}
             </span>
             <h3>{props.name}</h3>
