8a3a52e150592a9d9a7324817d9a78b61fc55cd9
Ethan Hasbrouck <ethanhasbrouck02@gmail.com>
2022-09-13 15:06:25 -0400

    Modify server/index.js

--- server/index.js
+++ server/index.js
@@ -8,14 +8,23 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use(express.json({ limit: "30mb", extended: true }));
-app.use(express.urlencoded({ limit: "30mb", extended: true}));
+app.use(express.json({ 
+    limit: "30mb", 
+    extended: true 
+}));
+app.use(express.urlencoded({ 
+    limit: "30mb", 
+    extended: true
+}));
 app.use(cors());
 app.use('/gaming-general', postRoutes);
 const CONNECTION_URL = process.env.MONGO_URL;
 
 const PORT = process.env.PORT || 5000;
-
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("----[ MongoDB connection verified ]----");
+});
 mongoose.connect(CONNECTION_URL)
-        .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}!`)))
+        .then(() => app.listen(PORT, () => console.log(`Server running on port ===> ${PORT}!`)))
         .catch((error) => console.log);
